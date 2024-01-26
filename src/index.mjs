5c7806ac8cecac2d2df2a5e6eef256d10fe71bf6
Mackiee21 <markpandac02@gmail.com>
2024-01-26 19:48:46 +0800

    demonyo na backtick

--- src/index.mjs
+++ src/index.mjs
@@ -17,7 +17,7 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/dist`')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(loginRouter);
 
 app.use(cookieParser());
@@ -33,7 +33,6 @@ app.get('/api', (req, res) => {
 app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 })
-
 //CAT ALL NOT FOUND ROUTES Redirect to NOT FOUND PAGE
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'NotFound.html'));

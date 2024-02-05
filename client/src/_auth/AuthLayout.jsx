c1c5f6d3ffd2d39437f4730dd11e9bfba7dbafe7
Mackiee21 <markpandac02@gmail.com>
2024-02-05 12:23:11 +0800

    balo oi

--- client/src/_auth/AuthLayout.jsx
+++ client/src/_auth/AuthLayout.jsx
@@ -8,8 +8,8 @@ function AuthLayout() {
     window.location.href = "/"
   }
   return (
-      <div id="hero" className="min-h-dvh flex flex-col bg-fixed">
-        <section className="flex-1 w-full flex items-center px-7 md:px-28 py-10">
+      <div id="hero" className="h-dvh flex flex-col bg-fixed">
+        <section className="flex-1 h-full w-full flex md:items-center px-7 md:px-28 py-16 md:py-10">
           <Outlet />
         </section>
       </div>

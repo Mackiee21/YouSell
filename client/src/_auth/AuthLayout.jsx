407700b86985e4e18f7452863b2cbf1a52996141
Mackiee21 <markpandac02@gmail.com>
2024-02-06 16:26:21 +0800

    pisit nga bwesit

--- client/src/_auth/AuthLayout.jsx
+++ client/src/_auth/AuthLayout.jsx
@@ -8,7 +8,7 @@ function AuthLayout() {
     window.location.href = "/"
   }
   return (
-      <div id="hero" style={{height: '100dvh'}}>
+      <div id="hero" className="min-h-dvh">
         <section className="h-full flex md:items-center px-7 md:px-28 py-16 md:py-10">
           <Outlet />
         </section>

600e975ad737e86d3e1738ae1b95fabab21e4783
Mackiee21 <markpandac02@gmail.com>
2024-02-06 16:46:13 +0800

    okay na unta

--- client/src/_auth/AuthLayout.jsx
+++ client/src/_auth/AuthLayout.jsx
@@ -8,7 +8,7 @@ function AuthLayout() {
     window.location.href = "/"
   }
   return (
-      <div id="hero" className="min-h-dvh">
+      <div id="hero" className="layout">
         <section className="h-full flex md:items-center px-7 md:px-28 py-16 md:py-10">
           <Outlet />
         </section>

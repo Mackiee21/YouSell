80018345a95d8b1809d8ca90f887522415d1ed17
Mackiee21 <markpandac02@gmail.com>
2024-02-06 17:09:20 +0800

    fixed height

--- client/src/_auth/AuthLayout.jsx
+++ client/src/_auth/AuthLayout.jsx
@@ -8,7 +8,7 @@ function AuthLayout() {
     window.location.href = "/"
   }
   return (
-      <div id="hero" className="layout">
+      <div id="hero" className="h-dvh">
         <section className="h-full flex md:items-center px-7 md:px-28 py-16 md:py-10">
           <Outlet />
         </section>
